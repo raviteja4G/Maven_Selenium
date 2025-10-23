b92e12ad5ea9747da9950f914d43d5128422240f
raviteja4G <tejarg3@gmail.com>
2025-10-23 21:10:50 +0530

    checkout

--- src/test/java/Tests/Test_001_Launch_and_Close_Browser_flow.java
+++ src/test/java/Tests/Test_001_Launch_and_Close_Browser_flow.java
@@ -9,7 +9,7 @@ public class Test_001_Launch_and_Close_Browser_flow {
 	@Test
 	public static void main(String[] args) {
 			
-		uihelper.launchUrl("https://www.google.com");
+		uihelper.launchUrl("https://www.google.com ");
 		uihelper.closer();
 		
 	}
